9dd726a3b7c4acde279d07adeb523605895e872b
OneComputers <uxasanov798@gmail.com>
2023-03-11 19:05:55 +0500

    Model and emus

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         //Umidjon
-        System.out.println("Hello world!");
+        System.out.println();
 
     }
 }
