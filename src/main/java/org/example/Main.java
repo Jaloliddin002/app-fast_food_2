e45a0072903d5a5def9fe8bd83a96e6841671d04
acer <sardorbeksaydullayev731@gmail.com>
2023-03-11 18:13:52 -0700

    hello

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,10 +2,8 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        int a = 4;
         //Umidjon
         System.out.println("Hello world!");
-        //Mnetta
-        String s = "Hello Worg`rld!";
+
     }
 }
